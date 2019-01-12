239652432c66f116650c1ced3b8c55a6d50d9c0a
Alec Deason <alec50@tinycountry.com>
2019-01-12 07:49:38 -0800

    More renderable svg

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ use svgdom::{Attribute, AttributeValue, Document, ElementId, Transform, ViewBox,
 fn main() {
     let mut generator = face_generator::Generator::new(&Path::new("assets"));
 
-    let width = 6;
-    let height = 3;
+    let total_width = 6.0 * 210.0;
+    let total_height = 3.0 * 210.0;
+    let x_count = 6;
+    let y_count = 3;
+    let width = total_width / x_count as f64;
+    let height = total_height / y_count as f64;
 
     let mut doc = Document::new();
     let mut svg = doc.create_element(ElementId::Svg);
@@ -19,21 +23,40 @@ fn main() {
         AttributeValue::ViewBox(ViewBox::new(
             0.0,
             0.0,
-            (width as f64) * 210.0,
-            (height as f64) * 210.0,
+            total_width,
+            total_height,
         )),
     ));
+    svg.set_attribute(Attribute::new("x", AttributeValue::Number(0.0)));
+    svg.set_attribute(Attribute::new("y", AttributeValue::Number(0.0)));
+    svg.set_attribute(Attribute::new("width", AttributeValue::Number(total_width)));
+    svg.set_attribute(Attribute::new("height", AttributeValue::Number(total_height)));
 
-    let faces: Vec<Document> = (0..width * height).map(|_| generator.generate()).collect();
+    let faces: Vec<Document> = (0..x_count * y_count).map(|_| generator.generate()).collect();
 
-    for x in 0..width {
-        for y in 0..height {
-            let face = &faces[x + y * width];
+    for x in 0..x_count {
+        for y in 0..y_count {
+            let face = &faces[x + y * x_count];
+            /*
             let transform = Transform::new_translate(x as f64 * 210.0, y as f64 * 210.0);
             face.svg_element().unwrap().set_attribute(Attribute::new(
                 "transform",
                 AttributeValue::Transform(transform),
             ));
+            */
+            face.svg_element().unwrap().set_attribute(Attribute::new(
+                "viewbox",
+                AttributeValue::ViewBox(ViewBox::new(
+                    0.0,
+                    0.0,
+                    width,
+                    height,
+                )),
+            ));
+            face.svg_element().unwrap().set_attribute(Attribute::new("x", AttributeValue::Number(x as f64 * 210.0)));
+            face.svg_element().unwrap().set_attribute(Attribute::new("y", AttributeValue::Number(y as f64 * 210.0)));
+            face.svg_element().unwrap().set_attribute(Attribute::new("width", AttributeValue::Number(width)));
+            face.svg_element().unwrap().set_attribute(Attribute::new("height", AttributeValue::Number(height)));
             svg.append(face.root());
         }
     }
